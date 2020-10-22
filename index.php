589eab5d094d3857de91f7b422b00ab84dc22482
icositetrachoron-programmer <69394951+icositetrachoron-programmer@users.noreply.github.com>
2020-10-22 16:17:12 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,4 +4,5 @@ logActivity("visited the homepage of the wiki");
 echo "Wiki made with PHP <br>";
 echo "Go to /article.php?article=articlename to view a article <br>";
 echo "Go to /edit.php to edit a page";
+echo "Go to /list_of_articles.php to view the index of articles <br>";
 ?>
